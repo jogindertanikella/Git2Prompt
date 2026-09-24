2981bbd569fa0a775c7be94ca14cbb5e11cdd5b7
Joginder Tanikella <joginder.tanikella@gmail.com>
2026-09-24 13:50:25 +0530

    Replace Citizen404 embed with verified ClueHopper rabbit

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,7 @@ function MainApp() {
     fetchSpinCount(setSpinCount);
   }, []);
 
+  /* Citizen404 paused.
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://citizen404.com/c.js";
@@ -68,6 +69,7 @@ function MainApp() {
       script.remove();
     };
   }, []);
+  */
 
   useEffect(() => {
     const handleKey = (e) => {
